dfec817a91936557b59ae0a1f7bba83c40016d54
Malavikamalu123 <103183741+Malavikamalu123@users.noreply.github.com>
2022-08-22 01:50:30 -0700

    Add files via upload

---
+++ multiply two array.py
@@ -0,0 +1,9 @@
+import numpy as np
+nums1=np.array([[2,5,2],[1,5,5]])
+nums2=np.array([[5,3,4],[3,2,5]])
+print("array1:")
+print(nums1)
+print("array2:")
+print(nums2)
+print("\nmultiply said array of same element-by-element:")
+print(np.multiply(nums1,nums2))
