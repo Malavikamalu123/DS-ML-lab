f13bc26325b1ec640e0b7047af973249e4202c6d
Malavikamalu123 <103183741+Malavikamalu123@users.noreply.github.com>
2022-08-22 02:50:40 -0700

    Add files via upload

---
+++ exp2.5.py
@@ -0,0 +1,21 @@
+import numpy as np
+def create_matrix(mc):
+    print("\nEnter the array"+str(mc)+"Elements:")
+    array_1=map(int,input().split())
+    array_1=np.array(list(array_1))
+    #print(arr)
+    print("\nEnter the array" + str(mc) + "row column:")
+    row,column=map(int,input().split())
+    if(len(array_1)!=(row*column)):
+        print("\nrow and column size not match with total elements!! retry")
+        return create_matrix(mc)
+    array_1 = array_1.reshape(row, column)
+    print("\narray"+str(mc))
+    print(array_1)
+    print("\nDeterminant : ")
+    return array_1
+print(np.linalg.det(create_matrix(1)))
+
+
+
+
---
+++ exp2.6.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+def create_matrix(mc):
+    print("\nEnter the array"+str(mc)+"Elements:")
+    array_1=map(int,input().split())
+    array_1=np.array(list(array_1))
+    print("\nEnter the array" + str(mc) + "row column:")
+    row,column=map(int,input().split())
+    if(len(array_1)!=(row*column)):
+        print("\nrow and column size not match with total elements!! retry")
+        return create_matrix(mc)
+    array_1 = array_1.reshape(row, column)
+    print("\narray"+str(mc))
+    print(array_1)
